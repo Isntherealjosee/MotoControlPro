6200f1067fe5c58d80134dcd4904097e17c53f53
Isntherealjosee <isntherealjose@icloud.com>
2026-06-13 19:33:26 +0000

    Fix: Cambio a urllib para parseo seguro de base de datos

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
-# Configuraciones de la base de datos MySQL
+import os
+from urllib.parse import urlparse
+
+# Buscamos la URL en Render. Si no existe, usamos la local.
+URL_BD = os.environ.get('DATABASE_URL', 'mysql://root:Joseesoto11-@127.0.0.1:3306/motocontrol_db')
+
+# Usamos la herramienta nativa de Python (la misma que funcionó en construir_nube.py)
+url = urlparse(URL_BD)
+
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'izarraras',         # Tu nuevo usuario
-    'password': 'SOIE07032p',    # Tu nueva contraseña
-    'database': 'motocontrol_db'
+    'host': url.hostname,
+    'user': url.username,
+    'password': url.password,
+    'database': url.path[1:],  # El [1:] quita la diagonal '/' del nombre de la base
+    'port': url.port
 }
